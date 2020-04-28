dbc12b0a9ea5e78576cc3256b702acbbccca8181
hamzagamal <54688005+hamza94max@users.noreply.github.com>
2020-04-29 00:57:29 +0200

    branch

--- app/src/main/java/com/example/counter/MainActivity.java
+++ app/src/main/java/com/example/counter/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.counter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -25,16 +26,33 @@ public class MainActivity extends AppCompatActivity {
         textView=findViewById(R.id.countertext);
         reset=findViewById(R.id.reset);
         RelativeLayout relativeLayout=findViewById(R.id.reltivee);
+
+
+
+
+
         relativeLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 counter++;
+
+
                 textView.setText(Integer.toString(counter));
+
+                if (counter >= 0 && counter < 33) {
+                    textView.setTextColor(Color.BLACK);
+                } else if (counter >= 33 && counter < 66) {
+                    //counter is bigger or equal than 33 AND smaller than 66
+                    //33, 34, ... 64, 65
+                    textView.setTextColor(Color.RED);
+                } else if (counter >= 66 && counter < 99) {
+                    textView.setTextColor(Color.YELLOW);
+                } else if (counter >= 99) {
+                    textView.setTextColor(Color.BLUE);
+                }
+
             }
         });
-
-
-
 
 
 
@@ -44,6 +62,9 @@ public class MainActivity extends AppCompatActivity {
 
                 counter = 0;
                 textView.setText(Integer.toString(counter));
+                textView.setTextColor(Color.BLACK);
+
+
 
 
 
@@ -53,11 +74,11 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    }
+    }}
 
 
 
 
 
 
-}
+
